15a4acd48cbf41a461800edd854d124225f8d500
Yitong Chen <142132200+cyteena@users.noreply.github.com>
2025-02-17 13:50:52 +0800

    feat: 添加 CMake 配置和 DArray 类的初始化功能

--- Homeworks/0_cpp_warmup/project/src/executables/1_BasicDArray/DArray.cpp
+++ Homeworks/0_cpp_warmup/project/src/executables/1_BasicDArray/DArray.cpp
@@ -1,5 +1,7 @@
 // implementation of class DArray
+#include <iostream>
 #include "DArray.h"
+#include <string>
 
 // default constructor
 DArray::DArray() {
@@ -9,10 +11,20 @@ DArray::DArray() {
 // set an array with default values
 DArray::DArray(int nSize, double dValue) {
 	//TODO
+	Init();
+	SetSize(nSize);
+	for (int i = 0; i < nSize; i++) {
+		SetAt(i, dValue);
+	}
 }
 
 DArray::DArray(const DArray& arr) {
 	//TODO
+	Init();
+	SetSize(arr.GetSize()); // copy the size
+	for (int i = 0; i< arr.GetSize(); i++) {
+		SetAt(i, arr.GetAt(i)); // copy the elements
+	}
 }
 
 // deconstructor
@@ -23,27 +35,51 @@ DArray::~DArray() {
 // display the elements of the array
 void DArray::Print() const {
 	//TODO
+	for (int i = 0; i < GetSize(); i++) {
+		std::cout << GetAt(i) << " ";	
+	}
 }
 
 // initilize the array
 void DArray::Init() {
 	//TODO
+	m_pData = NULL;
+	m_nSize = 0;
 }
 
 // free the array
 void DArray::Free() {
 	//TODO
+	if (m_pData != NULL) {
+		delete[] m_pData;
+		m_pData = NULL;	
+	}
 }
 
 // get the size of the array
 int DArray::GetSize() const {
 	//TODO
-	return 0; // you should return a correct value
+	return m_nSize; // you should return a correct value
 }
 
 // set the size of the array
 void DArray::SetSize(int nSize) {
 	//TODO
+	if (nSize == m_nSize){
+		return;
+	}
+	double* newData = new double[nSize];
+	int copysize = (nSize < m_nSize) ? nSize : m_nSize;
+	for (int i = 0; i < copysize; i++) {
+		newData[i] = m_pData[i];
+	}
+
+	if (m_pData != NULL) {
+		delete[] m_pData;
+	}
+
+	m_pData = newData;
+	m_nSize = nSize;
 }
 
 // get an element at an index
